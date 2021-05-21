39134c9b0f0f019b96fbbfe041d507c215042248
Tyler Reddy <tyler.je.reddy@gmail.com>
2021-05-20 20:07:02 -0600

    Starting rust hausdorff project

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn directed_hausdorff<T>(ar1: &[T], ar2: &[T]) -> T {
+	let mut cmax = 0;
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
