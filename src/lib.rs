1584be25f79cfa2a0d9740391ada989ad224c67d
Tyler Reddy <tyler.je.reddy@gmail.com>
2021-05-27 16:58:41 -0600

    ENH: start using ndarray crate
    * directed_hausdorff() now accepts 2D arrays from the `ndarray` crate,
    making it much more similar to the SciPy implementation
    
    * start taking advantage of the ability to iterate over the rows
    of the above 2D arrays to calculate the value of `d`
    
    * adjust the unit test to reflect the new implementation with
    `ndarray`
    
    * a few minor changes suggested by `cargo clippy`

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
-//extern crate num_traits;
-//use num_traits::Num;
+use ndarray::Array2;
 
-pub fn directed_hausdorff(ar1: &[f64], ar2: &[f64]) -> f64
+pub fn directed_hausdorff(ar1: Array2<f64>, ar2: Array2<f64>) -> f64
 {
     
     let inf = f64::INFINITY;
     let mut d = 0.0;
     let mut cmax = 0.0;
-
-	for &item_i in ar1.iter() {
+    let num_dims = ar1.shape()[1];
+    
+    println!("num_dims {}", num_dims);
+	for row_i in ar1.outer_iter() {
         let mut cmin = inf;
-        for &item_j in ar2.iter() {
-            d += &item_i + item_j;
+        for row_j in ar2.outer_iter() {
+            for dim in 0..num_dims {
+                d += row_i[dim] + row_j[dim];
+            }
             if d < cmax {
                 break;
             }
@@ -19,10 +22,8 @@ pub fn directed_hausdorff(ar1: &[f64], ar2: &[f64]) -> f64
                 cmin = d;
             }
         }
-    if cmin >= cmax {
-        if d >= cmax{
-            cmax = cmin;
-        }
+    if cmin >= cmax && d >= cmax {
+        cmax = cmin;
     }
     }
 d
@@ -33,10 +34,13 @@ d
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::prelude::*;
     #[test]
     fn it_works() {
-        let a1 = vec![1.0, 2.0, 3.0];
-        let a2 = vec![1.0, 2.0, 3.0];
-        assert_eq!(directed_hausdorff(&a1, &a2), 36.0);
+        let a1 = arr2(&[[1., 2., 3.],
+                        [4., 5., 6.]]);
+        let a2 = arr2(&[[1., 2., 3.],
+                        [4., 5., 6.]]);
+        assert_eq!(directed_hausdorff(a1, a2), 84.0);
     }
 }
