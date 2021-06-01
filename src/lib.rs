28277c1f34d149c39c3bce9f937b946754e4058c
Tyler Reddy <tyler.je.reddy@gmail.com>
2021-05-31 20:00:02 -0600

    ENH, MAINT: use actual distance
    * the `d` value in `directed_hausdorff()` is now calculated
    in the inner loop as a square distanced, and returned as
    a true distance
    
    * the single unit test now produces a sensible Hausdorff distance
    between two identical arrays (`0.0`) and the test has therefore
    been adjusted and renamed accordingly
    
    * remove a debug `println` no longer in use

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,18 @@ pub fn directed_hausdorff(ar1: Array2<f64>, ar2: Array2<f64>) -> f64
 {
     
     let inf = f64::INFINITY;
-    let mut d = 0.0;
     let mut cmax = 0.0;
+    let mut d = 0.0;
     let num_dims = ar1.shape()[1];
     
-    println!("num_dims {}", num_dims);
 	for row_i in ar1.outer_iter() {
         let mut cmin = inf;
         for row_j in ar2.outer_iter() {
+            d = 0.0;
             for dim in 0..num_dims {
-                d += row_i[dim] + row_j[dim];
+                // square of distance -- avoid sqrt
+                // until very end for performance
+                d += (row_i[dim] - row_j[dim]).powi(2);
             }
             if d < cmax {
                 break;
@@ -26,7 +28,7 @@ pub fn directed_hausdorff(ar1: Array2<f64>, ar2: Array2<f64>) -> f64
         cmax = cmin;
     }
     }
-d
+d.sqrt()
 }
 
 
@@ -36,11 +38,13 @@ mod tests {
     use super::*;
     use ndarray::prelude::*;
     #[test]
-    fn it_works() {
+    fn identical_arrays() {
+        // the directed Hausdorff distance between
+        // identical arrays should always be zero
         let a1 = arr2(&[[1., 2., 3.],
                         [4., 5., 6.]]);
         let a2 = arr2(&[[1., 2., 3.],
                         [4., 5., 6.]]);
-        assert_eq!(directed_hausdorff(a1, a2), 84.0);
+        assert_eq!(directed_hausdorff(a1, a2), 0.0);
     }
 }
