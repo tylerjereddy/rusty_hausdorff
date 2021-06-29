e5e6be9e5470780fb9960fe49b675dbc0ee4ceb9
Tyler Reddy <tyler.je.reddy@gmail.com>
2021-06-28 18:29:46 -0600

    MAINT: add core algo function
    * add function `directed_hausdorff_core()` so that it
    may be called separately in serial vs. multi-threaded
    scenarios as needed

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,22 @@ pub fn directed_hausdorff(
     ar2: &Array2<f64>,
     workers: u64,
 ) -> (f64, usize, usize) {
+    // TODO: decide if we're going to use random
+    // shuffling in the Rust version to match
+    // the SciPy implementation?
+
+    if workers == 0 {
+        // single thread/serial approach
+        directed_hausdorff_core(ar1, ar2)
+    } else {
+        // TODO: parallel implementation where
+        // each spawned thread gets a slice
+        // of ar1
+        (1.0, 1, 1)
+    }
+}
+
+fn directed_hausdorff_core(ar1: &Array2<f64>, ar2: &Array2<f64>) -> (f64, usize, usize) {
     let mut cmax = 0.0;
     let mut d = 0.0;
     let num_dims = ar1.shape()[1];
@@ -13,35 +29,28 @@ pub fn directed_hausdorff(
     let mut i_ret = 0;
     let mut j_ret = 0;
 
-    // TODO: decide if we're going to use random
-    // shuffling in the Rust version to match
-    // the SciPy implementation?
-
-    if workers == 0 {
-        // single thread/serial approach
-        for (i, row_i) in ar1.outer_iter().enumerate() {
-            let mut cmin = f64::INFINITY;
-            for (j, row_j) in ar2.outer_iter().enumerate() {
-                d = 0.0;
-                for dim in 0..num_dims {
-                    // square of distance -- avoid sqrt
-                    // until very end for performance
-                    d += (row_i[dim] - row_j[dim]).powi(2);
-                }
-                if d < cmax {
-                    break;
-                }
-                if d < cmin {
-                    cmin = d;
-                    i_store = i;
-                    j_store = j;
-                }
+    for (i, row_i) in ar1.outer_iter().enumerate() {
+        let mut cmin = f64::INFINITY;
+        for (j, row_j) in ar2.outer_iter().enumerate() {
+            d = 0.0;
+            for dim in 0..num_dims {
+                // square of distance -- avoid sqrt
+                // until very end for performance
+                d += (row_i[dim] - row_j[dim]).powi(2);
             }
-            if cmin >= cmax && d >= cmax {
-                cmax = cmin;
-                i_ret = i_store;
-                j_ret = j_store;
+            if d < cmax {
+                break;
             }
+            if d < cmin {
+                cmin = d;
+                i_store = i;
+                j_store = j;
+            }
+        }
+        if cmin >= cmax && d >= cmax {
+            cmax = cmin;
+            i_ret = i_store;
+            j_ret = j_store;
         }
     }
     (cmax.sqrt(), i_ret, j_ret)
